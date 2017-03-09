3ef86ea9d7492cc52c2ad5c911f694ce72b71182
orla <14031833@studentmail.ul.ie>
2017-03-09 14:34:35 +0000

    attempted to add validate password, did not work

--- changepassword.php
+++ changepassword.php
@@ -157,5 +157,90 @@
 
 <!-- END MENU -->
 
+<!--<script>
+$(document).ready(function(){
+    $('.tt-query').css('background-color', '#fff');
+
+              //CHECKS ALL INPUTS (WHEN BLURRED) WITHIN FORM ELEMENTS ON PAGE
+            $('form input').blur(function(){
+              //GETS THE ID OF ELEMENT JUST BLURRED
+              id = $(this).attr("id");
+              // IF WE ARE IN ONE OF THE PASSWORD FIELDS
+              else if(id.indexOf("pass") != -1){
+                // IF ITS THE FIRST, CHECK THAT IT MEETS MINIMUM PASSWORD REQUIREMENTS
+                  if(id.indexOf("pass1") != -1){
+
+                    if( $('#pass1Form').val().length >= 7){
+                      successInput(this);
+                      return true;
+                    }else{
+                      failInput(this);
+                      return false;
+                    }
+                  }
+                  if(id.indexOf("pass2") != -1){
+                    if(validateInput(this)){
+                    var pass = $('#pass1Form').val();
+                    var repass = $('#pass2Form').val();
+                    // passwords are not equal
+                  if(pass == repass){
+                    successInput(this);
+                    return false;
+                  }else{
+                    failInput(this);
+                    return true;
+                  }
+                }else{
+                  failInput(this);
+                }
+
+                }
+
+                else{
+
+                }
+              }
+              // IF ITS NOT A TAG OR PASSWORD OR EMAIL WE NEED TO CHECK IF IT IS ENTERED
+              else{
+                validateInput(this);
+              }
+            });
+            // END OF ONBLUR CHECKING OF FORM
+
+            // VERIFYING THAT THERE iS TEXT INPUT IN INPUTS
+            function validateInput(element){
+              id = element.id;
+              if(!$(element).val()){
+                failInput(element);
+                return false;
+              }
+              else {
+
+                  successInput(element);
+                  return true;
+
+              }
+            }
+            function failInput(element){
+              id = element.id;
+              var div = $("#" + id).closest("div");
+              div.removeClass("has-success");
+              $("#glypcn" + id).remove();
+              div.addClass("has-error has-feedback");
+              div.append('<span id="glypcn' + id + '" class="glyphicon glyphicon-remove form-control-feedback"></span>');
+            }
+
+            function successInput(element){
+              id = element.id;
+              var div = $("#" + id).closest("div");
+              div.removeClass("has-error");
+              $("#glypcn" + id).remove();
+              div.addClass("has-success has-feedback");
+              div.append('<span id="glypcn' + id + '" class="glyphicon glyphicon-ok form-control-feedback"></span>');
+            }
+          });
+        </script> -->
+
+
 </body>
 </html>
