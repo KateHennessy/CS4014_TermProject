0a28a50a23e9a6ae8a2db3b68df2e34a8f116ddc
KateHennessy <Kate-hennessy@outlook.com>
2017-03-11 11:17:36 +0000

    Minor change to one of the tag options

--- index.php
+++ index.php
@@ -16,11 +16,12 @@
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js">
     </script>
 
-    <!-- ONLINE BOOTSTRAP SELECT FILES -->
+    <!-- ONLINE BOOTSTRAP SELECT FILES (FOR TAGS SELECTION) -->
     <!-- Latest compiled and minified CSS -->
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.12.2/css/bootstrap-select.min.css">
 <!-- Latest compiled and minified JavaScript -->
 <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.12.2/js/bootstrap-select.min.js"></script>
+
 
     <!-- LOCAL BOOTSTRAP FILES  -->
     <!-- <link rel="stylesheet" href="bootstrap/bootstrap.css">
