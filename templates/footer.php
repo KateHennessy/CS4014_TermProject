30525dbe6cc12cc5f765e0910360998ae3fae04c
orla <14031833@studentmail.ul.ie>
2017-03-16 14:44:39 +0000

    Changed footer to look more like the nav bar and altered css accordinglly (took out previous footer css)

--- templates/footer.php
+++ templates/footer.php
@@ -1,26 +1,25 @@
-<div class="container text-center well">
-
-  <div class="row">
-    <div class="col-md-12">
-      <div class="col-md-4">
-        <ul class="nav nav-pills nav-stacked">
-          <h4><div class="glyphicon glyphicon-eye-open"></div></h4>
-          <li><a href="<?php echo 'aboutus.php'; ?>">About us</a></li>
-        </ul>
+<nav class="navbar navbar-default navbar-inverse" role="navigation">
+  <div class="container-fluid background-image">
+    <!-- Brand and toggle get grouped for better mobile display -->
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+        <span class="sr-only">Toggle navigation
+        </span>
+        <span class="icon-bar">
+        </span>
+        <span class="icon-bar">
+        </span>
+        <span class="icon-bar">
+        </span>
+      </button>
+        <a rel="icon" type="image/ico" href="images/icon.ico"></a>
+        <a class="navbar-brand" href="#">RevIUL</a>
+        <a class="navbar-brand" href="aboutus.php">About Us</a>
+        <a class="navbar-brand" href="information.php">Information</a>
       </div>
-      <div class="col-md-4">
-        <ul class="nav nav-pills nav-stacked">
-            <h4><div class="glyphicon glyphicon-folder-open"></div></h4>
-          <li><a href="<?php echo 'information.php'; ?>">Information</a></li>
-        </ul>
-      </div>
-      <div class="col-md-4">
-        <ul class="nav nav-pills nav-stacked">
-          <br />
-          <li>Copyright 2017</li>
-        </ul>
-      </div>
+      <ul class="nav navbar-nav navbar-right">
+        <li>
+          <p class="navbar-text">CopyRight 2017</p>
+        </li>
     </div>
-  </div>
-
-</div>
+  </nav>
