3d63574b84cb8a7bcd949eeb8f3ff6fcf2f6174e
orla <14031833@studentmail.ul.ie>
2017-03-16 15:18:53 +0000

    Altered about us page and also attempted to place footer on bottom of every page - I still need to work on that element

--- templates/footer.php
+++ templates/footer.php
@@ -1,25 +1,12 @@
+<footer>
 <nav class="navbar navbar-default navbar-inverse" role="navigation">
   <div class="container-fluid background-image">
     <!-- Brand and toggle get grouped for better mobile display -->
-    <div class="navbar-header">
-      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-        <span class="sr-only">Toggle navigation
-        </span>
-        <span class="icon-bar">
-        </span>
-        <span class="icon-bar">
-        </span>
-        <span class="icon-bar">
-        </span>
-      </button>
-        <a rel="icon" type="image/ico" href="images/icon.ico"></a>
-        <a class="navbar-brand" href="#">RevIUL</a>
-        <a class="navbar-brand" href="aboutus.php">About Us</a>
-        <a class="navbar-brand" href="information.php">Information</a>
-      </div>
-      <ul class="nav navbar-nav navbar-right">
-        <li>
-          <p class="navbar-text">CopyRight 2017</p>
-        </li>
+    <div>
+        <div class="col-sm-3"><a class="navbar-brand" href="profilepage.php">RevIUL</a></div>
+        <div class="col-sm-3"><a class="navbar-brand" href="aboutus.php">About Us</a></div>
+        <div class="col-sm-3"><a class="navbar-brand" href="information.php">Information</a></div>
+        <div class="col-sm-3"><p class="navbar-text">CopyRight 2017</p></div>
     </div>
   </nav>
+</footer>
