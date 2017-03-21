7264f5002e54bf5254979e0c51c7aa6237c6aafa
orla <14031833@studentmail.ul.ie>
2017-03-21 13:00:20 +0000

    changed name of change password to account settings, added success message to change password and log in to about us & info pages

--- information.php
+++ information.php
@@ -1,23 +1,40 @@
-<?php
-  session_start();
-
-    if (isset($_SESSION["user_id"]) && $_SESSION["user_id"] != ''){
-      $id = $_SESSION["user_id"];
-	   require_once __DIR__.'/templates/loggedinuser.php';
-      // echo("ID: " .$id);
-    } else {
-      // echo("In else " .$_SESSION["user_id"]);
-         require_once __DIR__.'/templates/header.template.php';
-    }
-	?>
 <!DOCTYPE html>
 <html lang="en">
   <head>
     <title>ReviUL-Information
     </title>
+
+
 <?php
+  session_start();
     require_once __DIR__.'/models/User.class.php';
-    require_once __DIR__.'/models/Tag.class.php';
+    require_once __DIR__.'/daos/UserDAO.class.php';
+    require_once __DIR__."/utils/Settings.class.php";
+    require_once __DIR__."/database/DatabaseQueries.php";
+    require_once __DIR__."/utils/PDOAccess.class.php";
+
+    if(isset($_POST["login_button"])){
+     $email = trim(strtolower($_POST["email"]));
+     $password = $_POST["password"];
+     $user = new User();
+     $user = UserDAO::login($email, $password);
+
+     if(!is_null($user)){
+       $_SESSION["user_id"] = $user->get_id();
+      header("location:./profilepage.php");
+     }else{
+       header("location:./register.php");
+     }
+   }
+
+   if (isset($_SESSION["user_id"]) && $_SESSION["user_id"] != ''){
+     $id = $_SESSION["user_id"];
+    require_once __DIR__.'/templates/loggedinuser.php';
+     // echo("ID: " .$id);
+   } else {
+     // echo("In else " .$_SESSION["user_id"]);
+        require_once __DIR__.'/templates/header.template.php';
+   }
     ?>
 
 
