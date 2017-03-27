e365872a183564812d1e971fb0e0d3a79a1707da
orla <14031833@studentmail.ul.ie>
2017-03-27 09:31:07 +0100

    claimed task stream now working dynamically

--- myclaimedtasks.php
+++ myclaimedtasks.php
@@ -12,6 +12,7 @@
               $userDao = new UserDAO();
               $user = $userDao->getUserByID($id);
               $uploadedTasks = TaskDAO::find_user_uploaded_tasks($user);
+              $claimedTasks = TASKDAO::find_user_claimed_tasks($user->get_id());
               // print_r($uploadedTasks);
 
             } else {
@@ -54,10 +55,17 @@
                     <div class="row">
                         <div class="col-sm-6 col-md-12">
                             <h2> My  Claimed Tasks </h2>
-                            <p>Tasks I have claimed</p>
+                            <p>Tasks that I have claimed</p>
                         </div>
                     </div>
                     <br />
+
+                    <?php
+                    if(count($claimedTasks) == 0) { ?>
+                      <h5 class="col-sm-6 text-primary"> No tasks claimed. To claim one <a href="<?php echo 'availabletasks.php'?>"> <em>Click here </em></a></h5> 
+                    <?php }
+
+                    foreach($claimedTasks as $task){ ?>
 
                     <!-- Begin Task1-->
                     <div class="col-sm-6 col-lg-4">
@@ -68,55 +76,50 @@
                                         <a class="pull-left" href="<?php echo 'detailedtask.php'; ?>" target="_parent">
 
                                             <!-- </a> -->
-                                            <h4><div class="glyphicon glyphicon-edit"></div>The Study of Pineapples</h4></a>
+                                            <h4><div class="glyphicon glyphicon-edit"></div><?php echo $task->get_title(); ?></h4></a>
                                     </div>
                                     <div class="pull-right hidden-xs col-sm-4">
-                                        <h4><small class="pull-right">Assignment</small></h4>
+                                        <h4><small class="pull-right"><?php echo $task->get_type(); ?></small></h4>
                                     </div>
                                 </div>
                                 <ul class="list-inline">
-                                    <li>PDF</li>
+                                    <li><?php echo $task->get_format(); ?></li>
                                     <li style="list-style: none">|</li>
-                                    <li>10 Pages</li>
+                                    <li><?php echo $task->get_no_pages(); ?> Pages</li>
                                     <li style="list-style: none">|</li>
-                                    <li>7000 Words</li>
+                                    <li><?php echo $task->get_no_words(); ?> Words</li>
                                 </ul>
-                                <p class="hidden-xs fixedBody">A study on the growing habitats of pineapples </p>
-                                <div><label for="Success" class="btn btn-success">Completed</label></div>
+                                <p class="hidden-xs fixedBody"><?php echo $task->get_description(); ?> </p>
+                                <?php
+                                switch($task->get_status()->get_name()){
+                                  case "unclaimed":
+                                  echo '<div><label for="primary" class="btn btn-info">Not Claimed</label></div>';
+                                  break;
+                                  case "in progress":
+                                  echo '<div><label for="warning" class="btn btn-warning">In Progress</label></div>';
+                                  break;
+                                  case "expired":
+                                  echo '<div><label for="danger" class="btn btn-danger">Expired</label></div>';
+                                  break;
+                                  case "cancelled":
+                                  echo '<div><label for="danger" class="btn btn-danger">Cancelled</label></div>';
+                                  break;
+                                  case "unfinished":
+                                  echo '<div><label for="danger" class="btn btn-danger">Unfinished</label></div>';
+                                  break;
+                                  default:
+                                  echo '';
+                                  break;
+                                }
+                                ?>
                             </div>
                         </div>
-                    </div>
+                    </div><?php } ?>
                     <!-- End Task1-->
 
-                    <!-- Begin Task2-->
-                    <div class="col-sm-6 col-lg-4">
-                        <div class="panel panel-default">
-                            <div class="panel-heading fixed">
-                                <div class="row">
-                                    <div class="col-xs-12 col-sm-8">
-                                        <a class="pull-left" href="<?php echo 'detailedtask.php'; ?>" target="_parent">
-                                            <h4><div class="glyphicon glyphicon-edit"></div>Are Video Games Bad?</h4>
-                                        </a>
-                                    </div>
-                                    <div class="pull-right hidden-xs col-sm-4">
-                                        <h4><small class="pull-right">PhD Thesis</small></h4>
-                                    </div>
-                                </div>
-                                <ul class="list-inline">
-                                    <li>Docx</li>
-                                    <li style="list-style: none">|</li>
-                                    <li>35000 Words</li>
-                                    <li style="list-style: none">|</li>
-                                    <li>100 Pages</li>
-                                </ul>
-                                <p class="hidden-xs fixedBody"> A study investigating the side effects of violent video games on children</p>
-                                <div><label for="Warning" class="btn btn-warning">In Progress</label></div>
-                                <br />
-                            </div>
-                        </div>
-                    </div>
 
-                    <!-- End Task2-->
+                    <!--End Claimed Tasks-->
+
                   </div>
                 </div>
               </div>
