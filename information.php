7cf530593d97f6a05ab5275715bf868ce7c8998a
orla <14031833@studentmail.ul.ie>
2017-04-15 16:12:02 +0100

    Changed info page to a FAQ page with jquery accordian - also did page in relation to no javascript so answers will appear if javascript is disabled

--- information.php
+++ information.php
@@ -22,7 +22,7 @@
      <!DOCTYPE html>
      <html lang="en">
        <head>
-         <title>ReviUL-Information
+         <title>ReviUL-FAQ
          </title>
 
            <?php
@@ -36,24 +36,55 @@
         <div class="col-xs-11 col-sm-8 well">
             <div class="profile-content">
                 <?php echo $feedback; ?>
-                <h1><div class="glyphicon glyphicon-info-sign"></div>General Information</h1><br>
-                <h3>User Information</h3>
+                <h1><div class="glyphicon glyphicon-info-sign"></div>Frequently Asked Questions</h1><br>
+                <noscript>
+                  <div class="alert alert-warning">
+                   <p>  <i class="glyphicon glyphicon-alert"></i> Please enable javascript for best user experience</p>
+                 </div>
+               </noscript>
+
+                <button class ="accordion">Who can join this website?</button>
+                <div class ="panel">
                   <p align = "justify">
                     Students and staff at the University of Limerick can join ReviUL to avail of a free and easy proof reading service. User's must be a member of the University of Limerick with a valid UL email.
                   </p>
                   <p align = "justify">
                     Once a user has registered, they can upload tasks for other people to proof read. They also have the ability to proof read other members tasks.
                   </p>
+                  </div>
+                  <noscript>
+                    <h3>Who can join this website?</h3>
+                    <p align = "justify">
+                      Students and staff at the University of Limerick can join ReviUL to avail of a free and easy proof reading service. User's must be a member of the University of Limerick with a valid UL email.
+                    </p>
+                    <p align = "justify">
+                      Once a user has registered, they can upload tasks for other people to proof read. They also have the ability to proof read other members tasks.
+                    </p>
+                  </noscript>
 
-                <h3>Reputation Points</h3>
+
+
+                <button class ="accordion">What are reputation points?</button>
+                <div class ="panel">
                   <p align = "justify">
                     Members of the website can gain reputation points by claiming a task to proof read and also flagging any task as inappropriate. However, user's can also lose reputation points if they cancel a task or also if they fail to submit a task before the due date.
                   </p>
                   <p align = "justify">
                     Once a user has obtained enough reputation points, they will be afforded the ability to review flagged tasks by other users. However, if points are deducted from the user in future, their ability to review tasks may be taken away until they have once again reached enough reputation points.
                   </p>
+                </div>
+                <noscript>
+                  <h3>What are reputation points?</h3>
+                  <p align = "justify">
+                    Members of the website can gain reputation points by claiming a task to proof read and also flagging any task as inappropriate. However, user's can also lose reputation points if they cancel a task or also if they fail to submit a task before the due date.
+                  </p>
+                  <p align = "justify">
+                    Once a user has obtained enough reputation points, they will be afforded the ability to review flagged tasks by other users. However, if points are deducted from the user in future, their ability to review tasks may be taken away until they have once again reached enough reputation points.
+                  </p>
+                </noscript>
 
-                <h3>Task Information</h3>
+                <button class ="accordion">Why do I need to upload tasks?</button>
+                <div class ="panel">
                   <p align = "justify">
                     User's can upload tasks they wish to have proof read by another user. These tasks can include a thesis or assignment. These tasks can also be across a range of disciplines. The file uploaded must also be a PDF or a docx.
                   </p>
@@ -69,8 +100,28 @@
                   <p align = "justify">
                     Your profile page will contain a snippet of information on tasks you have uploaded. It will not show every task you have ever uploaded or claimed. To see such tasks please go ton the "My Tasks" or "My Claimed Tasks" in the side bar.
                   </p>
+                </div>
+                <noscript>
+                  <h3>Why do I need to upload tasks?</h3>
+                  <p align = "justify">
+                    User's can upload tasks they wish to have proof read by another user. These tasks can include a thesis or assignment. These tasks can also be across a range of disciplines. The file uploaded must also be a PDF or a docx.
+                  </p>
+                  <p align = "justify">
+                    When uploading a task, users must provide relevant information on their task. This information can include document type, word count, page count, title of task and a snippet of the content in the task.
+                  </p>
+                  <p align = "justify">
+                    Users can search for tasks they wish to claim to proof read for other user. Once a user has found a task they wish to proof read, they can message the user who uploaded that task on their profile page. In this message, a user can state what task they wish to claim and also include their email address so the user can reply back with the task in question.
+                  </p>
+                  <p align = "justify">
+                    When uploading a sample of your task, please ensure it is not the entire document. There are file size resitrictions in place.
+                  </p>
+                  <p align = "justify">
+                    Your profile page will contain a snippet of information on tasks you have uploaded. It will not show every task you have ever uploaded or claimed. To see such tasks please go ton the "My Tasks" or "My Claimed Tasks" in the side bar.
+                  </p>
+                </noscript>
 
-                <h3>Claiming a Task</h3>
+                <button class ="accordion">How can I claim a task to proof read it?</button>
+                <div class ="panel">
                   <p align = "justify">
                     You can claim a task by clicking on the task title. Once you do this you will be brought to a detailed task page with a claim button on the bottom of the screen.
                   </p>
@@ -80,8 +131,22 @@
                   <p align = "justify">
                     The date the task in question must be completed by is in the detailed task view. You can see this date before claiming the task. Therefore, all users are aware of the timeline in which the task must be proof read by.
                   </p>
+                </div>
+                <noscript>
+                  <h3>How can I claim a task to proof read it?</h3>
+                  <p align = "justify">
+                    You can claim a task by clicking on the task title. Once you do this you will be brought to a detailed task page with a claim button on the bottom of the screen.
+                  </p>
+                  <p align = "justify">
+                    Once the claim button has been clicked you have claimed the task. The user who upladed the task will be given your email address to send you a full copy of their document. This email address will appear in the detailed task view once the task is marked as completed.
+                  </p>
+                  <p align = "justify">
+                    The date the task in question must be completed by is in the detailed task view. You can see this date before claiming the task. Therefore, all users are aware of the timeline in which the task must be proof read by.
+                  </p>
+                </noscript>
 
-                <h3>Updating Task Status</h3>
+                <button class ="accordion">How can I update task information(task status)?</button>
+                <div class ="panel">
                   <p align = "justify">
                     Updating the status of a task is important once you have claimed it. You can update the status to cancelled or completed.
                   </p>
@@ -91,8 +156,22 @@
                   <p align = "justify">
                     It is important to update your status on claimed tasks, as this could affect your reputation score.
                   </p>
+                </div>
+                <noscript>
+                  <h3>How can I update task information(task status)?</h3>
+                  <p align = "justify">
+                    Updating the status of a task is important once you have claimed it. You can update the status to cancelled or completed.
+                  </p>
+                  <p align = "justify">
+                    Once you have updated the status of the task, the user will be made aware of the changing of the status via their profile page or "My Task" stream. Once you claim a task it will automatically be set to "In Progress". You can then update it to cancelled or completed. Please note the user you uploaded the task will be aware when it is completed as the status will change, and if not done so they can state as such on the rating system.
+                  </p>
+                  <p align = "justify">
+                    It is important to update your status on claimed tasks, as this could affect your reputation score.
+                  </p>
+                </noscript>
 
-                <h3>Banning of Users</h3>
+                <button class ="accordion">Why are some users banned?</button>
+                <div class ="panel">
                   <p align = "justify">
                     If a user uploads content which is deemed inappropriate by another user, other users have the option to flag that task.
                   </p>
@@ -102,6 +181,20 @@
                   <p align = "justify">
                     Once a user has been banned, they cannot use the website. They will be unable to log in or register for the website.
                   </p>
+                </div>
+                <noscript>
+                  <h3>Why are some users banned?</h3>
+                  <p align = "justify">
+                    If a user uploads content which is deemed inappropriate by another user, other users have the option to flag that task.
+                  </p>
+                  <p align = "justify">
+                    Once a task is flagged, it will be reviewed. After the reviewal, if the result is that it is inappropriate, the task will be taken down, the users account will be deactivated and the user will be banned from registering from ReviUL in future.
+                  </p>
+                  <p align = "justify">
+                    Once a user has been banned, they cannot use the website. They will be unable to log in or register for the website.
+                  </p>
+                </noscript>
+
         </div>
         </div>
       </div>
@@ -109,6 +202,28 @@
   </div>
 </div>
 
+<script>
+
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+    acc[i].onclick = function(){
+        /* Toggle between adding and removing the "active" class,
+        to highlight the button that controls the panel */
+        this.classList.toggle("active");
+
+        /* Toggle between hiding and showing the active panel */
+        var panel = this.nextElementSibling;
+        if (panel.style.display === "block") {
+            panel.style.display = "none";
+        } else {
+            panel.style.display = "block";
+        }
+    }
+}
+
+</script>
 
 
         <?php
